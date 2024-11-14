a2de06651234efcc5170d488161fe10cbbe4fd06
Emmanouil <manoliskastanidis@hotmail.com.br>
2024-11-14 21:33:09 +0200

    Added cost functions' gradients

--- main/cost_functions.py
+++ main/cost_functions.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 # Cost functions
-def l1_error_function(y, y_pred):
+def l1_error(y, y_pred):
     return np.sum(np.abs(y - y_pred))
 
-def l2_error_function(y, y_pred):
+def l2_error(y, y_pred):
     return np.sum(np.power(y - y_pred, 2))
 
-def cross_entropy_loss_error_function(y, y_pred):
+def cross_entropy_loss(y, y_pred):
     return -np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
 
 def mean_squared_error(y, y_pred):
@@ -16,10 +16,33 @@ def mean_squared_error(y, y_pred):
 def mean_absolute_error(y, y_pred):
     return np.mean(np.abs(y - y_pred))
 
-def binary_crossentropy(y, y_pred):
+def binary_crossentropy_error(y, y_pred):
     return np.mean(-y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred))
 
-def hinge_loss_error_function(y, y_pred):
-    return np.mean(np.maximum(0, 1 - y * y_pred))
+#TODO: Implement hinge loss error function
+def hinge_loss_error(y, y_pred):
+    pass
 
-# more to come
+# Gradients of cost functions
+
+# I did the maths on my own, hopefully it's correct. :D
+def l1_error_gradient(y, y_pred):
+    return np.sum((y_pred - y) / np.abs(y - y_pred))
+
+def l2_error_gradient(y, y_pred):
+    return np.sum(2 * (y_pred - y))
+
+def cross_entropy_loss_gradient(y, y_pred):
+    return np.sum(y / y_pred - (y - 1) / (y_pred - 1))
+
+def mean_squared_error_gradient(y, y_pred):
+    return np.mean(2 * (y_pred - y))
+
+def mean_absolute_error_gradient(y, y_pred):
+    return np.mean((y_pred - y) / np.abs(y - y_pred))
+
+def binary_crossentropy_gradient(y, y_pred):
+    return np.mean(y / y_pred - (y - 1) / (y_pred - 1))
+
+def hinge_loss_gradient(y, y_pred):
+    pass    
