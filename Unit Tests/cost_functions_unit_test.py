ded3c7f4ad58cd570b740d15a8520cc732495526
Emmanouil <manoliskastanidis@hotmail.com.br>
2024-11-14 23:40:50 +0200

    renamed functions

--- Unit Tests/cost_functions_unit_test.py
+++ Unit Tests/cost_functions_unit_test.py
@@ -4,58 +4,105 @@ import sys
 import os
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../main')))
 
-from main.cost_functions import (
-    l1_error_function,
-    l2_error_function,
-    cross_entropy_loss_error_function,
-    mean_squared_error,
-    mean_absolute_error,
-    binary_crossentropy,
-    hinge_loss_error_function
+
+from activation_functions import (
+    linear_activation, binary_step_activation, ReLU_activation, PReLU_activation,
+    ELU_activation, SLU_activation, softplus_activation, logistic_activation,
+    tanh_activation, arctan_activation, linear_activation_gradient,
+    binary_step_activation_gradient, ReLU_activation_gradient,
+    PReLU_activation_gradient, ELU_activation_gradient,
+    SLU_activation_gradient, logistic_activation_gradient,
+    softplus_activation_gradient, tanh_activation_gradient,
+    arctan_activation_gradient
 )
 
-class TestCostFunctions(unittest.TestCase):
+class TestActivationFunctions(unittest.TestCase):
 
-    def setUp(self):
-        self.y = np.array([1, 0, 1, 0])
-        self.y_pred = np.array([0.9, 0.1, 0.8, 0.2])
+    def test_linear_activation(self):
+        self.assertEqual(linear_activation(5), 5)
+        self.assertEqual(linear_activation(-3), -3)
 
-    def test_l1_error_function(self):
-        result = l1_error_function(self.y, self.y_pred)
-        expected = np.sum(np.abs(self.y - self.y_pred))
-        self.assertAlmostEqual(result, expected)
+    def test_binary_step_activation(self):
+        self.assertEqual(binary_step_activation(5), 1)
+        self.assertEqual(binary_step_activation(-3), 0)
 
-    def test_l2_error_function(self):
-        result = l2_error_function(self.y, self.y_pred)
-        expected = np.sum(np.power(self.y - self.y_pred, 2))
-        self.assertAlmostEqual(result, expected)
+    def test_ReLU_activation(self):
+        self.assertEqual(ReLU_activation(5), 5)
+        self.assertEqual(ReLU_activation(-3), 0)
 
-    def test_cross_entropy_loss_error_function(self):
-        result = cross_entropy_loss_error_function(self.y, self.y_pred)
-        expected = -np.sum(self.y * np.log(self.y_pred) + (1 - self.y) * np.log(1 - self.y_pred))
-        self.assertAlmostEqual(result, expected)
+    def test_PReLU_activation(self):
+        self.assertEqual(PReLU_activation(5), 5)
+        self.assertEqual(PReLU_activation(-3), -0.03)
+        self.assertEqual(PReLU_activation(-3, alpha=0.1), -0.3)
 
-    def test_mean_squared_error(self):
-        result = mean_squared_error(self.y, self.y_pred)
-        expected = np.mean(np.power(self.y - self.y_pred, 2))
-        self.assertAlmostEqual(result, expected)
+    def test_ELU_activation(self):
+        self.assertEqual(ELU_activation(5), 5)
+        self.assertAlmostEqual(ELU_activation(-3), -0.95021293)
+        self.assertAlmostEqual(ELU_activation(-3, alpha=0.5), -0.47510646)
 
-    def test_mean_absolute_error(self):
-        result = mean_absolute_error(self.y, self.y_pred)
-        expected = np.mean(np.abs(self.y - self.y_pred))
-        self.assertAlmostEqual(result, expected)
+    def test_SLU_activation(self):
+        self.assertAlmostEqual(SLU_activation(5), 4.966527196)
+        self.assertAlmostEqual(SLU_activation(-3), -0.952574127)
 
-    def test_binary_crossentropy(self):
-        result = binary_crossentropy(self.y, self.y_pred)
-        expected = np.mean(-self.y * np.log(self.y_pred) - (1 - self.y) * np.log(1 - self.y_pred))
-        self.assertAlmostEqual(result, expected)
+    def test_softplus_activation(self):
+        self.assertAlmostEqual(softplus_activation(5), 5.006715348)
+        self.assertAlmostEqual(softplus_activation(-3), 0.048587351)
 
-    def test_hinge_loss_error_function(self):
-        result = hinge_loss_error_function(self.y, self.y_pred)
-        expected = np.mean(np.maximum(0, 1 - self.y * self.y_pred))
-        self.assertAlmostEqual(result, expected)
+    def test_logistic_activation(self):
+        self.assertAlmostEqual(logistic_activation(5), 0.993307149)
+        self.assertAlmostEqual(logistic_activation(-3), 0.047425873)
+
+    def test_tanh_activation(self):
+        self.assertAlmostEqual(tanh_activation(5), 0.999909204)
+        self.assertAlmostEqual(tanh_activation(-3), -0.995054754)
+
+    def test_arctan_activation(self):
+        self.assertAlmostEqual(arctan_activation(0), 1.373400767)
+        self.assertAlmostEqual(arctan_activation(-3), -1.249045772)
+
+    def test_linear_activation_gradient(self):
+        self.assertEqual(linear_activation_gradient(5), 1)
+        self.assertEqual(linear_activation_gradient(-3), 1)
+
+    def test_binary_step_activation_gradient(self):
+        self.assertEqual(binary_step_activation_gradient(5), 0)
+        self.assertEqual(binary_step_activation_gradient(-3), 0)
+
+    def test_ReLU_activation_gradient(self):
+        self.assertEqual(ReLU_activation_gradient(5), 1)
+        self.assertEqual(ReLU_activation_gradient(-3), 0)
+
+    def test_PReLU_activation_gradient(self):
+        self.assertEqual(PReLU_activation_gradient(5), 1)
+        self.assertEqual(PReLU_activation_gradient(-3), 0.01)
+        self.assertEqual(PReLU_activation_gradient(-3, alpha=0.1), 0.1)
+
+    def test_ELU_activation_gradient(self):
+        self.assertEqual(ELU_activation_gradient(5), 1)
+        self.assertAlmostEqual(ELU_activation_gradient(-3), 0.049787068)
+        self.assertAlmostEqual(ELU_activation_gradient(-3, alpha=0.5), 0.024893534)
+
+    def test_SLU_activation_gradient(self):
+        self.assertAlmostEqual(SLU_activation_gradient(5), 1.02654743243)
+        self.assertAlmostEqual(SLU_activation_gradient(-3), 0.002466509)
+
+    def test_logistic_activation_gradient(self):
+        self.assertAlmostEqual(logistic_activation_gradient(5), 0.006648056)
+        self.assertAlmostEqual(logistic_activation_gradient(-3), 0.002466509)
+
+    def test_softplus_activation_gradient(self):
+        self.assertAlmostEqual(softplus_activation_gradient(5), 0.993307149)
+        self.assertAlmostEqual(softplus_activation_gradient(-3), 0.047425873)
+
+    def test_tanh_activation_gradient(self):
+        self.assertAlmostEqual(tanh_activation_gradient(5), 0.000181583)
+        self.assertAlmostEqual(tanh_activation_gradient(-3), 0.009866037)
+
+    def test_arctan_activation_gradient(self):
+        self.assertAlmostEqual(arctan_activation_gradient(0), 1.0)
+        self.assertAlmostEqual(arctan_activation_gradient(-3), 0.1)
 
 if __name__ == '__main__':
     unittest.main()
--- main/activation_functions.py
+++ main/activation_functions.py
@@ -39,34 +39,34 @@ def arctan_activation(x):
     return np.arctan(x)
 
 
-# Derivatives of cost functions
+# Gradients of cost functions
 
-def linear_activation_derivative(x):
+def linear_activation_gradient(x):
     return 1
 
-def binary_step_activation_derivative(x):
+def binary_step_activation_gradient(x):
     return 0
 
-def ReLU_activation_derivative(x):
+def ReLU_activation_gradient(x):
     return 1 if x >= 0 else 0
 
-def PReLU_activation_derivative(x, alpha=0.01):
+def PReLU_activation_gradient(x, alpha=0.01):
     return 1 if x >= 0 else alpha
 
-def ELU_activation_derivative(x, alpha=1.0):
+def ELU_activation_gradient(x, alpha=1.0):
     return 1 if x >= 0 else alpha * np.exp(x)
 
-def SLU_activation_derivative(x, c=1.0):
+def SLU_activation_gradient(x, c=1.0):
     return np.exp(c*x) * (np.exp(c*x) + c*x + 1) / (np.exp(c*x) + 1)**2
 
-def logistic_activation_derivative(x, c=1.0):
+def logistic_activation_gradient(x, c=1.0):
     return c * np.exp(-c*x) / (1 + np.exp(-c*x))**2
 
-def softplus_activation_derivative(x):
+def softplus_activation_gradient(x):
     return 1 / (1 + np.exp(-x))
 
-def tanh_activation_derivative(x):
+def tanh_activation_gradient(x):
     return 1 - np.tanh(x)**2
 
-def arctan_activation_derivative(x):
+def arctan_activation_gradient(x):
     return 1 / (x**2 + 1)
